9c5f38f3f568feb901e3cdaed038c0039baac3c8
IzHoBX <hobingxuan97@gmail.com>
2018-05-31 20:17:40 +0800

    Adds Facebook login authentication

---
+++ app/src/main/java/com/izho/callappassignment/LoginActivity.java
@@ -0,0 +1,48 @@
+package com.izho.callappassignment;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton facebookLoginButton = findViewById(R.id.login_button);
+
+        facebookLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
--- app/src/main/java/com/izho/callappassignment/MainActivity.java
+++ app/src/main/java/com/izho/callappassignment/MainActivity.java
@@ -1,7 +1,15 @@
 package com.izho.callappassignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if(!(accessToken != null && !accessToken.isExpired())) {
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        }
+
     }
 }
